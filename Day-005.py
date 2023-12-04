d4fcd0f8f66d214c9075ccdb5df35d5f4cd77546
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2023-12-04 18:31:06 +0530

    Create Day-005.py

---
+++ Day-005.py
@@ -0,0 +1,10 @@
+'''
+hey Anjali, don't remove this line.
+Author:Anjali Singh
+100Days of code
+'''
+
+print('Hello Everyone!,'
+      "This is my first python code\nand this is very useful course.")
+print(12, sep="~", end="200\n")
+print(10)
