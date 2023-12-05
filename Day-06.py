eafda62ba9707ca6a829a22f542f733c4563ec22
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2023-12-05 23:13:47 +0530

    Create Day-06.py

---
+++ Day-06.py
@@ -0,0 +1,20 @@
+a = 1.1
+print(a)
+Harry = 9
+b = Harry
+print(b)
+c = True
+d = None
+print(c)
+print(d)
+a1 = 9
+print(a+a1)
+print("The type pf a is ",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+
+list1 = [8,2,3,[-4,5],["apple","banana"]]
+print(list1)
+
+tuple1 = (("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
