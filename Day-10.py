27f2ed0527a2b438b9ea69ea7e10649c7b1b48b4
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2023-12-05 23:16:03 +0530

    Create Day-10.py

---
+++ Day-10.py
@@ -0,0 +1,13 @@
+a = input("Enter the number:")
+b = input("Enter the number:")
+
+print(a + b)
+print(int(a) + int(b))
+
+print("The addition is", int(a) + int(b))
+print("The subtractions is", int(a) - int(b))
+print("The multiplication is", int(a) * int(b))
+print("The divisions is", int(a) / int(b))
+print("The mudulus is", int(a) % int(b))
+print("The floor is", int(a) // int(b))
+print("The power is", int(a)**int(b))
