4c7a45c2c21d2204e858fc8487687507093f778a
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2023-12-10 18:56:48 +0530

    Add files via upload

---
+++ Day-012/Day-012.py
@@ -0,0 +1,20 @@
+# string slicing
+names = "Anjali,Nandani"
+print(names[0:6])
+
+stringlen = print(len(names))
+
+fruit = "mango"
+print(fruit[0:4])
+print(fruit[0:5])
+print(fruit[:])
+print(fruit[:5])
+
+# Negative slicing
+print(fruit[:-3])
+print(fruit[0:len(fruit)-4])
+print(fruit[-1:-2])
+print(fruit[-4:-1])
+
+name = "Harry"
+print(name[-4:-2])
