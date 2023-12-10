42886f50e826dffd9bc34cb1e6e7d7adff81eb59
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2023-12-10 18:55:09 +0530

    Add files via upload

---
+++ Day-011/Day-011.py
@@ -0,0 +1,38 @@
+name = "Anjali"
+friend = "Harry"
+anotherFriend = "Nandani"
+apple = '''He said,
+Hi Anjali
+hey I am good 
+"I want to eat an apple"'''
+
+str = """Hii Anjali,
+       I am good.
+       How's about you?
+       I hope you also good.
+
+       I just want to know about you that what's your plan
+       this weekend?
+
+       can we go to maldives this year for some extra cellular fun 
+       activities."""
+print(str)
+
+print("Hello" + " " + name)
+print(friend[0])
+print(friend[1])
+print(friend[2])
+print(friend[3])
+print(friend[4])
+# print(friend[5])
+
+print("Let's use a for loop\n")
+
+for characters in name:
+     print(characters)
+
+print()
+
+for characters in str:
+     print(characters)
+  
