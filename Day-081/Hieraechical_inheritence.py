a8b7e243efa5b37cef0167b53bf3c070baf1773a
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2024-01-04 06:28:30 +0530

    Add files via upload

---
+++ Day-081/Hieraechical_inheritence.py
@@ -0,0 +1,11 @@
+class BaseClass:
+     pass
+
+class D1(BaseClass):
+     pass
+
+class D2(BaseClass):
+     pass
+
+class D3(D1):
+     pass
---
+++ Day-081/Hybrid.py
@@ -0,0 +1,11 @@
+class BaseClass:
+     pass
+
+class DerivedClass1(BaseClass):
+     pass
+
+class DerivedClass2(BaseClass):
+     pass
+
+class Derivedclass3(DerivedClass1,DerivedClass2):
+     pass
